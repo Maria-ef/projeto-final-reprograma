17a51eddecef021c5a4cd4e0c57d8f4b2c031918
Maria-ef <maria.eferreira99@gmail.com>
2023-07-24 22:21:28 -0300

    continuacao home
    array e estilizacao dos cards

--- maria-eduarda/projeto-react/src/components/CardHome.jsx
+++ maria-eduarda/projeto-react/src/components/CardHome.jsx
@@ -1,8 +1,10 @@
+import styles from '../styles/components/cardhome.module.css'
+
 const CardHome = ({title, description}) => {
     return (
         <div className="cardsHome">
-          <h1>{title}</h1>
-          <p>{description}</p>
+          <h1 className={styles.cardTitle}>{title}</h1>
+          <p className={styles.cardDescription}>{description}</p>
         </div>
     )
 }
--- maria-eduarda/projeto-react/src/components/Header.jsx
+++ maria-eduarda/projeto-react/src/components/Header.jsx
@@ -1,32 +1,16 @@
 import styles from "../styles/components/header.module.css";
 
-const Header = ({ title, image }) => {
+const Header = ({ title, image, text, description }) => {
   return (
     <header className={styles.headerContainer}>
       <div>
         <img className={styles.headerImage} src={image} alt={title} />
       </div>
-      <div className={styles.headerIntro}>
-        <h2 className={styles.headerTitle}>Bem-vindas ao nosso site "Mulheres na Tech"!</h2>
-        <p className={styles.headerDescription}>
-             A indústria da tecnologia tem sido historicamente dominada por homens,
-          mas estamos determinadas a mudar essa realidade. Acreditamos que cada
-          mulher possui um potencial incrível para impulsionar a inovação e
-          promover transformações significativas no mundo através da tecnologia.
-          Queremos quebrar barreiras, desafiar estereótipos e garantir que todas
-          as mulheres interessadas tenham a oportunidade de se destacar nesse
-          campo.
-        </p>
-        <br />
-        <p className={styles.headerDescription}>
-          Aqui, incentivamos mulheres a seguirem carreira na área tech. Neste
-          site, você encontrará informações valiosas para ajudá-la a iniciar e
-          progredir em sua jornada na área de tecnologia. Desde guias práticos
-          sobre como escolher a carreira certa até histórias inspiradoras de
-          mulheres bem-sucedidas na indústria. Nosso objetivo é fornecer
-          conhecimentos relevantes e ferramentas indispensáveis para o seu
-          sucesso.
-        </p>
+        
+      <div>
+        <h3 className={styles.headerTitle}>{title}</h3>
+        <p className={styles.headerText}>{text}</p>
+        <p className={styles.headerDescription}>{description}</p>
       </div>
     </header>
   );
